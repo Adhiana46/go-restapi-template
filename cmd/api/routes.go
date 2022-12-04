8d6657ffede1790d44f53db97f0dd12bcb3477ba
Adhiana Mastur <adhianamastur48@gmail.com>
2022-12-05 01:01:31 +0700

    default errorHandler, panic middleware, activityGroupHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,105 +1,32 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
-	"github.com/Adhiana46/go-restapi-template/internal/dto"
-	responsePkg "github.com/Adhiana46/go-restapi-template/pkg/response"
+	"github.com/Adhiana46/go-restapi-template/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func routes() *fiber.App {
-	r := fiber.New()
+	r := fiber.New(fiber.Config{
+		ErrorHandler: handleError,
+	})
+
+	// Handle Panic
+	r.Use(func(c *fiber.Ctx) error {
+		log.Println("Handle Panic Middleware")
+
+		defer handlePanic(c)
+
+		return c.Next()
+	})
 
 	api := r.Group("/api/v1")
-	{
-		activityGroupRoutes := api.Group("/activity_group")
-		{
-			activityGroupRoutes.Get(":uuid", func(c *fiber.Ctx) error {
-				defer handlePanic(c)
 
-				req := dto.ActivityGroupUuidRequest{}
-				if err := shouldBind(c, &req); err != nil {
-					panic(err)
-				}
-
-				activityGroup, err := svcActivityGroup.FindByUuid(req)
-				if err != nil {
-					return handleError(c, err)
-				}
-
-				resp := dto.ActivityGroupToResponse(activityGroup)
-
-				return c.JSON(responsePkg.JsonSuccess(http.StatusOK, "", resp, nil))
-			})
-			api.Get("activity_group", func(c *fiber.Ctx) error {
-				defer handlePanic(c)
-
-				req := dto.ActivityGroupFetchRequest{}
-				if err := shouldBind(c, &req); err != nil {
-					panic(err)
-				}
-
-				activityGroupList, pagination, err := svcActivityGroup.FetchAll(req)
-				if err != nil {
-					return handleError(c, err)
-				}
-
-				resp := dto.ActivityGroupToResponseList(activityGroupList)
-
-				return c.JSON(responsePkg.JsonSuccess(http.StatusOK, "", resp, pagination))
-			})
-			api.Post("activity_group", func(c *fiber.Ctx) error {
-				defer handlePanic(c)
-
-				req := dto.ActivityGroupCreateRequest{}
-				if err := shouldBind(c, &req); err != nil {
-					panic(err)
-				}
-
-				activityGroup, err := svcActivityGroup.Create(req)
-				if err != nil {
-					return handleError(c, err)
-				}
-
-				resp := dto.ActivityGroupToResponse(activityGroup)
-
-				return c.JSON(responsePkg.JsonSuccess(http.StatusOK, "", resp, nil))
-			})
-			api.Put("activity_group/:uuid", func(c *fiber.Ctx) error {
-				defer handlePanic(c)
-
-				req := dto.ActivityGroupUpdateRequest{}
-				if err := shouldBind(c, &req); err != nil {
-					panic(err)
-				}
-
-				activityGroup, err := svcActivityGroup.Update(req)
-				if err != nil {
-					return handleError(c, err)
-				}
-
-				resp := dto.ActivityGroupToResponse(activityGroup)
-
-				return c.JSON(responsePkg.JsonSuccess(http.StatusOK, "", resp, nil))
-			})
-			api.Delete("activity_group/:uuid", func(c *fiber.Ctx) error {
-				defer handlePanic(c)
-
-				req := dto.ActivityGroupUuidRequest{}
-				if err := shouldBind(c, &req); err != nil {
-					panic(err)
-				}
-
-				err := svcActivityGroup.Delete(req)
-				if err != nil {
-					return handleError(c, err)
-				}
-
-				return c.JSON(responsePkg.JsonSuccess(http.StatusNoContent, "", nil, nil))
-			})
-		}
-	}
+	// Register Handlers
+	handlers.
+		NewActivityGroupHandler(svcActivityGroup).
+		RegisterRoutes(api.Group("/activity_group"))
 
 	return r
 }
---
+++ handlers/activity_group.go
@@ -0,0 +1,134 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Adhiana46/go-restapi-template/internal/dto"
+	"github.com/Adhiana46/go-restapi-template/internal/service"
+	parserPkg "github.com/Adhiana46/go-restapi-template/pkg/parser"
+	responsePkg "github.com/Adhiana46/go-restapi-template/pkg/response"
+	"github.com/gofiber/fiber/v2"
+)
+
+type ActivityGroupHandler interface {
+	RegisterRoutes(r fiber.Router) ActivityGroupHandler
+
+	findByUuid() func(c *fiber.Ctx) error
+	fetchAll() func(c *fiber.Ctx) error
+	create() func(c *fiber.Ctx) error
+	update() func(c *fiber.Ctx) error
+	delete() func(c *fiber.Ctx) error
+}
+
+type activityGroupHandler struct {
+	svcActivityGroup service.ActivityGroupService
+}
+
+func NewActivityGroupHandler(svcActivityGroup service.ActivityGroupService) ActivityGroupHandler {
+	return &activityGroupHandler{
+		svcActivityGroup: svcActivityGroup,
+	}
+}
+
+func (h *activityGroupHandler) RegisterRoutes(r fiber.Router) ActivityGroupHandler {
+	r.Get("/:uuid", h.findByUuid())
+	r.Get("/", h.fetchAll())
+	r.Post("/", h.create())
+	r.Put("/:uuid", h.update())
+	r.Delete("/:uuid", h.delete())
+
+	return h
+}
+
+func (h *activityGroupHandler) findByUuid() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		req := dto.ActivityGroupUuidRequest{}
+		if err := parserPkg.FiberShouldBindRequest(c, &req); err != nil {
+			panic(err)
+		}
+
+		activityGroup, err := h.svcActivityGroup.FindByUuid(req)
+		if err != nil {
+			return err
+		}
+
+		resp := dto.ActivityGroupToResponse(activityGroup)
+
+		statusCode := http.StatusOK
+		return c.Status(statusCode).JSON(responsePkg.JsonSuccess(statusCode, "", resp, nil))
+	}
+}
+
+func (h *activityGroupHandler) fetchAll() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		req := dto.ActivityGroupFetchRequest{}
+		if err := parserPkg.FiberShouldBindRequest(c, &req); err != nil {
+			panic(err)
+		}
+
+		activityGroupList, pagination, err := h.svcActivityGroup.FetchAll(req)
+		if err != nil {
+			return err
+		}
+
+		resp := dto.ActivityGroupToResponseList(activityGroupList)
+
+		statusCode := http.StatusOK
+		return c.Status(statusCode).JSON(responsePkg.JsonSuccess(statusCode, "", resp, pagination))
+	}
+}
+
+func (h *activityGroupHandler) create() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		req := dto.ActivityGroupCreateRequest{}
+		if err := parserPkg.FiberShouldBindRequest(c, &req); err != nil {
+			panic(err)
+		}
+
+		activityGroup, err := h.svcActivityGroup.Create(req)
+		if err != nil {
+			return err
+		}
+
+		resp := dto.ActivityGroupToResponse(activityGroup)
+
+		statusCode := http.StatusOK
+		return c.Status(statusCode).JSON(responsePkg.JsonSuccess(statusCode, "", resp, nil))
+	}
+}
+
+func (h *activityGroupHandler) update() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		req := dto.ActivityGroupUpdateRequest{}
+		if err := parserPkg.FiberShouldBindRequest(c, &req); err != nil {
+			panic(err)
+		}
+
+		activityGroup, err := h.svcActivityGroup.Update(req)
+		if err != nil {
+			return err
+		}
+
+		resp := dto.ActivityGroupToResponse(activityGroup)
+
+		statusCode := http.StatusOK
+		return c.Status(statusCode).JSON(responsePkg.JsonSuccess(statusCode, "", resp, nil))
+	}
+}
+
+func (h *activityGroupHandler) delete() func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		req := dto.ActivityGroupUuidRequest{}
+		if err := parserPkg.FiberShouldBindRequest(c, &req); err != nil {
+			panic(err)
+		}
+
+		err := h.svcActivityGroup.Delete(req)
+		if err != nil {
+			return err
+		}
+
+		statusCode := http.StatusOK
+		return c.Status(statusCode).JSON(responsePkg.JsonSuccess(statusCode, "", nil, nil))
+	}
+}
